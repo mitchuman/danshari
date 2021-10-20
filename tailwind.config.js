2d80d7fba8a5ae8c6045fce75f3fffec5235f220
Mitchell Christ <mitchell@human.marketing>
2021-10-19 17:42:47 -0700

    add tailwind, start config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.{liquid,html}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
