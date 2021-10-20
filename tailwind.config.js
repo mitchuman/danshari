cd85c90de5abaa32b35541186250b1b15ae2a8b9
Mitchell Christ <mitchell@human.marketing>
2021-10-19 20:06:42 -0700

    update media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,30 @@ module.exports = {
   purge: ['./**/*.{liquid,html}'],
   darkMode: false,
   theme: {
-    fontFamily: {
-      'heading': ['var(--font-heading)'],
-      'body': ['var(--font-body)'],
+    screens: {
+      'mobile': {max: '640px'},
+      'mobile+': {min: '641px'},
+      'tablet': {max: '768px'},
+      'tablet+': {min: '769px'},
+      'desktop': {max: '1280px'},
+      'desktop+': {min: '1281px'},
     },
-    extend: {},
+    fontFamily: {
+      'heading': 'var(--font-heading)',
+      'body': 'var(--font-body)',
+    },
+    extend: {
+      colors: {
+        green: {
+          DEFAULT: 'var(--primary-color)',
+          dark: 'var(--primary-color-dark)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary-color)',
+          dark: 'var(--secondary-color-dark)',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
